9711339183c11e9145fe7c010aaba0750427083f
Ryan Kadri <ryankadri.dev@gmail.com>
2023-09-25 17:27:28 +0200

    :construction: WIP: GoalListFixtures

--- src/DataFixtures/GoalListFixtures.php
+++ src/DataFixtures/GoalListFixtures.php
@@ -3,19 +3,35 @@
 namespace App\DataFixtures;
 
 use App\Entity\GoalList;
+use App\Repository\LanguageRepository;
+use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class GoalListFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    )
+    {}
+
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create('en_US');
+
+        $languages = $this->entityManager->getRepository(LanguageRepository::class)->findAll();
+        $users = $this->entityManager->getRepository(UserRepository::class)->findAll();
+
+        // Create 20 fake goal lists
         for ($i = 1; $i <= 20; $i++) {
             $goalList = new GoalList();
-            /*
-
-            */
+            $goalList
+                ->setName($faker->sentence(3))
+                ->setUser($faker->randomElement($users))
+                ->setLanguage($faker->randomElement($languages));
             $manager->persist($goalList);
         }
 
